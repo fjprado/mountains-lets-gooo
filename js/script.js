60c73bb4fb4c9de73a66959654c1196116f7bb55
Fernando Prado <f.j.prado@hotmail.com>
2025-09-21 00:54:17 -0700

    Update data first blocks

--- js/script.js
+++ js/script.js
@@ -1,39 +1,43 @@
 let cypressDetails = {};
+const fullDateStyle = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+};
+const shortDateStyle = { year: 'numeric', month: 'long', day: 'numeric' };
 
 const fetchCypressDetails = async () => {
     //const response = await fetch('https://mountains-lets-gooo-api.onrender.com/api/cypress-data');
     //cypressDetails = await response.json();
     cypressDetails = cypressDataJson;
-    updateUI();
+    if (cypressDetails) {
+        document.getElementById('loading-screen').style.opacity = '0';
+        document.getElementById('content').style.display = 'block';
+        setTimeout(function () {
+            document.getElementById('loading-screen').style.display = 'none';
+        }, 500);
+        document.getElementById('last-update').textContent = cypressDetails.lastUpdate
+    }
 }
 
-const updateUI = () => {
-    // Set background color based on weather type
-    const body = document.body;
-    switch (cypressDetails.currentWeather.type) {
-        case 'clear-day':
-            body.style.backgroundColor = 'yellow';
-            break;
-        case 'clear-night':
-        case 'partly-cloudy-night':
-            body.style.backgroundColor = 'black';
-            break;            
-        case 'partly-cloudy-day':
-            body.style.backgroundColor = 'lightblue';
-            break;
-        case 'rain':
-        case 'sleet':
-            body.style.backgroundColor = 'white';
-            break;
-        case 'cloudy':
-        case 'snow':            
-        case 'wind':
-        case 'fog':
-            body.style.backgroundColor = 'lightgray';
-            break;
-        default:
-            body.style.backgroundColor = 'lightblue'; // Default background
+const initializePage = async () => {
+    await fetchCypressDetails();
+    if (cypressDetails) {
+        document.getElementById('last-update').textContent = new Date(cypressDetails.lastUpdate).toLocaleDateString('en-US', fullDateStyle);
+        document.getElementById('opening-date').textContent = new Date(cypressDetails.openingDate).toLocaleDateString('en-US', shortDateStyle);
+        document.getElementById('days-for-opening').textContent = cypressDetails.daysForOpening;
+        document.getElementById('current-weather-icon').src = cypressDetails.currentWeather.icon;
+        document.getElementById('current-weather-summary').textContent = cypressDetails.currentWeather.conditionsSummary + ', ' + cypressDetails.currentWeather.temperature + '°C';
+        document.getElementById('current-weather-wind').textContent = "Wind: " + cypressDetails.currentWeather.wind + ' km/h';
+        document.getElementById('current-weather-wind').textContent = "Wind: " + cypressDetails.currentWeather.wind + ' km/h';
+        document.getElementById('uphill-status').textContent = cypressDetails.operations.uphillStatus;
+        document.getElementById('open-run-percentage').textContent = cypressDetails.runsOpen;
+        document.getElementById('open-run').textContent = cypressDetails.runsOpenPercent;
     }
-};
+}
 
-fetchCypressDetails();
+
+
+initializePage();
